338246511ff854aceac8632eb1dd52d0f77cc157
thierry.kunda <thierry.kunda@universite-paris-saclay.fr>
2022-04-21 23:19:36 +0200

    Modele : grille de zone + ile

--- src/Model/Modele.java
+++ src/Model/Modele.java
@@ -2,5 +2,27 @@ package Model;
 import Observing.Observable;
 
 public class Modele extends Observable {
-    // A compléter
+    private static final int LONGUEUR = 4, LARGEUR = 4;
+    private Zone[][] zones;
+    private Ile ile;
+
+    public Modele() {
+        zones = new Zone[LONGUEUR][LARGEUR];
+        for (int i = 0; i < LONGUEUR; i++) {
+            for (int j = 0; j < LARGEUR; j++) {
+                zones[i][j] = new Zone(this, i, j);
+            }
+        }
+        ile = new Ile(this);
+    }
+
+    public Zone[][] getGrilleZones() {
+        return this.zones;
+    }
+
+    public Zone getZone(int x, int y) {
+        return this.zones[x][y];
+    }
+    
+    
 }

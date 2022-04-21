0d3be68841c9e44fcd30ac8f7e9d8e63ae7fb242
thierry.kunda <thierry.kunda@universite-paris-saclay.fr>
2022-04-21 16:35:54 +0200

    Création classe Ile

---
+++ src/Model/Ile.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Ile {
+    private Modele modele;
+    private ArrayList<Zone> zones;
+    
+    public Ile (Modele modele) {
+        this.modele = modele;
+    }
+    
+    
+}

6f00adf6c93295f43edaa049d285afd982bef2b8
chang-young.jung <chang-young.jung@universite-paris-saclay.fr>
2022-04-22 22:49:35 +0200

    Ajout init()

--- src/Model/Modele.java
+++ src/Model/Modele.java
@@ -1,8 +1,14 @@
 package Model;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import Observing.Observable;
+import outils.Element;
+import outils.NiveauEau;
 
 public class Modele extends Observable {
-    public static final int LARGEUR = 4, HAUTEUR = 4;
+    public static final int LARGEUR = 7, HAUTEUR = 7;
+    public static final int NbArtCle = 4;
     private Zone[][] zones;
     private Ile ile;
 
@@ -13,6 +19,7 @@ public class Modele extends Observable {
                 zones[i][j] = new Zone(this, i, j);
             }
         }
+        init();
         ile = new Ile(this);
     }
 
@@ -24,5 +31,49 @@ public class Modele extends Observable {
         return this.zones[x][y];
     }
     
-    
+    public void init(){
+        int nBArt = 4;
+        int nBKey = 4;
+        ArrayList<Element> Type = new ArrayList<Element>(Arrays.asList(Element.values()));
+        zones[0][0].heliZone();
+        
+        for(int i = 1; i < LARGEUR; i++){
+            for(int j = 0; j < HAUTEUR; j++){
+                double a = Math.random();
+                double b = Math.random();
+                double c = Math.random();
+                if (a < 0.4){
+                    zones[i][j].zoneType(NiveauEau.Normale);
+                    if( b <= 0.3 & nBArt > 0 ){
+                        zones[i][j].addArtefact(Type.get(nBArt));
+                        nBArt--;
+                    }
+                    if( c <= 0.5 & nBKey > 0){
+                        if(zones[i][j].contientArtef() == false){
+                            zones[i][j].addCle(Type.get(nBKey)));
+                            nBKey--;
+                        }
+                    }
+                }
+                else if (a <= 0.5 & a < 0.8 ){
+                    zones[i][j].zoneType(NiveauEau.Inondee);
+                    if( b < 0.3 & nBArt > 0){
+                          zones[i][j].addArtefact(Type.get(nBArt));
+                        nBArt--;
+                    }
+                    if( c <= 0.4 & nBKey > 0){
+                        if(zones[i][j].contientArtef() == false){
+                            zones[i][j].addCle(Type.get(nBKey)));
+                            nBKey--;
+                }
+                else {
+                    zones[i][j].zoneType(NiveauEau.Submergee);
+                }
+                }
+            }
+        }
+
+    }
 }
+}
+

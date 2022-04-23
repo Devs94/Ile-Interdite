02077ade4fbac26756e95220adcec550dd4111ef
chang-young.jung <chang-young.jung@universite-paris-saclay.fr>
2022-04-23 17:33:47 +0200

    MaJ Joueur

--- src/Model/Joueur.java
+++ src/Model/Joueur.java
@@ -2,7 +2,48 @@ package Model;
 
 import Model.Ile;
 import Model.Zone;
+import outils.Direction;
 
 public class Joueur {
+    private Modele modele;
+    private Zone position;
+    private Direction direction;
+
+
+    public Joueur(Modele modele, Zone zone){
+        this.modele = modele;
+        this.position = zone;
+    }
+
+    public Direction getDirection(){
+        return this.direction;
+    }
+
+    public void move(Direction key){
+        this.direction = key;
+        switch(this.direction){
+            case down: this.position = new Zone(this.position.x, this.position.y -1);
+                break;
+            case up: this.position = new Zone(this.position.x, this.position.y + 1);
+                break;
+            case left: this.position = new Zone(this.position.x - 1, this.position.y);
+                break;
+            case right: this.position = new Zone(this.position.x + 1, this.position.y);
+                break;
+            default:
+                break;
+        }
+    }
     
+
+
+
+
+
+
+
+
+
+
+
 }

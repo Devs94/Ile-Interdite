e9eafd5d52f73b92b0f9e8d9d058a692fc6fa492
chang-young.jung <chang-young.jung@universite-paris-saclay.fr>
2022-04-23 22:35:07 +0200

    MaJ Controleur

--- src/Controller/Controleur.java
+++ src/Controller/Controleur.java
@@ -1,12 +1,18 @@
 package Controller;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public abstract class Controleur implements ActionListener {
+import Model.Modele;
+
+public class Controleur implements ActionListener {
+    Modele modele;
+    public Controleur(Modele modele){
+        this.modele = modele;
+    }
 
     public void actionPerformed(ActionEvent e) {
-        // A compléter
-        
+        modele.EndtourInondation();        
     }
     
 }

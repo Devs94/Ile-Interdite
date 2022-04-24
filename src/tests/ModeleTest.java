4e7ed928578abdc20fdb9f1b4f5f0aeeb0d05a77
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2022-04-24 15:23:55 +0200

    Test : compte occurence niveau d'eau

--- src/tests/ModeleTest.java
+++ src/tests/ModeleTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 import Model.Modele;
+import Model.Zone;
 
 public class ModeleTest {
     Modele mTest;
@@ -16,5 +17,25 @@ public class ModeleTest {
     @Test
     public void zoneNiveauNotNull() {
         Assert.assertNotNull(mTest.getZone(0, 0).getNiveauEau());
+    }
+
+    @Test
+    public void compteZonesNiveauEau() {
+        int sec = 0, inond = 0, sub = 0;
+        Zone[][] zones = mTest.getGrilleZones();
+        for (int i = 0; i < zones.length; i++) {
+            for (int j = 0; j < zones[0].length; j++) {
+                switch (zones[i][j].getNiveauEau()) {
+                    case Normale: sec++;
+                        break;
+                    case Inondee: inond++;
+                        break;
+                    case Submergee: sub++;
+                    default:
+                        break;
+                }
+            }
+        }
+        System.out.printf("%d, %d, %d sur %d au total", sec, inond, sub, zones.length*zones[0].length);
     }
 }
